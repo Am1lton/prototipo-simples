75dbe4dc22287cf0b6cf02a48c79bb30a861cf43
Arthur Camargo <arthurcg2@gmail.com>
2025-04-24 17:01:35 -0300

    Added coins and score
    Added coins that move and rotate, when colected they increase the player score witch is shown in the upper corner of the screen

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Player;
+
+namespace Coin
+{
+    [RequireComponent(typeof(Collider))]
+    public class Coin : MonoBehaviour, ICollectable
+    {
+        private Vector3 startPosition;
+
+        private void Start() => startPosition = transform.position;
+
+        private void Update()
+        {
+            RotateInPlace();
+            
+            transform.position = startPosition + Vector3.up * (Mathf.Sin(Time.time * 2) * 0.2f + 0.7f);
+        }
+
+        private void RotateInPlace()
+        {
+            Vector3 rotation = transform.rotation.eulerAngles;
+            rotation.y += Time.deltaTime * 150;
+            transform.rotation = Quaternion.Euler(rotation);
+        }
+        
+        
+        #region ICollectable
+            public void OnCollect(Transform collector)
+            {
+                if (collector.TryGetComponent(out PlayerScore playerScore))
+                {
+                    playerScore.AddScore(1);
+                    Destroy(gameObject);
+                }
+            }
+
+            private void OnTriggerEnter(Collider other)
+            {
+                OnCollect(other.transform);
+            }
+        #endregion
+    }
+}
--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -1,54 +1,57 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody), typeof(Collider))]
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    private Rigidbody rb;
-    private Collider col;
-
-    [SerializeField] private float speed = 10f;
-    [SerializeField] private float jumpForce = 30f;
-    [SerializeField] private float extraGravity = 85f;
-
-    private LayerMask groundLayer;
-    private void Start()
+    [RequireComponent(typeof(Rigidbody), typeof(Collider))]
+    public class PlayerMovement : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody>();
+        private Rigidbody rb;
+        private Collider col;
+        private LayerMask groundLayer;
+
+        [SerializeField] private float speed = 10f;
+        [SerializeField] private float jumpForce = 30f;
+        [SerializeField] private float extraGravity = 85f;
         
-        rb.drag = 0f;
-        rb.angularDrag = 0f;
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody>();
+            
+            rb.drag = 0f;
+            rb.angularDrag = 0f;
+            
+            col = GetComponent<Collider>();
+            groundLayer = LayerMask.GetMask("Ground");
+        }
+
+        private void FixedUpdate()
+        {
+           float horizontalVelocity = Input.GetAxisRaw("Horizontal");
+           float verticalVelocity = Input.GetAxisRaw("Vertical");
+           
+           Vector3 moveInput = new Vector3(horizontalVelocity, 0, verticalVelocity).normalized * speed;
+           
+           rb.velocity = new Vector3(moveInput.x, rb.velocity.y, moveInput.z);
+
+           if (!IsOnGround())
+           {
+               rb.AddForce(Vector3.down * extraGravity, ForceMode.Acceleration);
+           }
+            
+        }
+        private void Update()
+        {
+
+           if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
+           {
+                rb.AddForce(0, jumpForce, 0, ForceMode.VelocityChange);
+           }
+        }
         
-        col = GetComponent<Collider>();
-        groundLayer = LayerMask.GetMask("Ground");
-    }
-
-    private void FixedUpdate()
-    {
-       float horizontalVelocity = Input.GetAxisRaw("Horizontal");
-       float verticalVelocity = Input.GetAxisRaw("Vertical");
-       
-       Vector3 moveInput = new Vector3(horizontalVelocity, 0, verticalVelocity).normalized * speed;
-       
-       rb.velocity = new Vector3(moveInput.x, rb.velocity.y, moveInput.z);
-
-       if (!IsOnGround())
-       {
-           rb.AddForce(Vector3.down * extraGravity, ForceMode.Acceleration);
-       }
-        
-    }
-    private void Update()
-    {
-
-       if (Input.GetKeyDown(KeyCode.Space) && IsOnGround())
-       {
-            rb.AddForce(0, jumpForce, 0, ForceMode.VelocityChange);
-       }
-    }
-
-    public bool IsOnGround()
-    {
-        float height = col.bounds.size.y;
-        return Physics.Raycast(transform.position, Vector3.down, height * 0.6f, groundLayer);
+        public bool IsOnGround()
+        {
+            float height = col.bounds.size.y;
+            return Physics.Raycast(transform.position, Vector3.down, height * 0.6f, groundLayer);
+        }
     }
 }
